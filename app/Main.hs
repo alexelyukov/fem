455f6adb624a939e98c586a62c3e0db136e76fdd
Alexelyukov <a.elyukov@timeweb.ru>
2021-01-10 01:41:44 +0300

    fix

--- app/Main.hs
+++ app/Main.hs
@@ -12,3 +12,11 @@ main = do
     drawLine (V2 300 300, V2 400 400) 1 (PixelRGBA8 0xFF 0x00 0x00 255) Solid
     drawLine (V2 300 300, V2 400 300) 1 (PixelRGBA8 0xFF 0x00 0x00 255) Dashed
     drawLines [(V2 600 600, V2 700 700), (V2 800 800, V2 900 900)] 3 (PixelRGBA8 0xFF 0x00 0x00 255) Dashed
+    drawRectangle (V2 500 500, V2 600 700) 3 (PixelRGBA8 0xFF 0x00 0x00 255) Solid
+    fillRectangle (V2 500 500, V2 600 700) (PixelRGBA8 0xAA 0x00 0x00 255)
+    drawCircle (V2 1000 1000, 200) 3 (PixelRGBA8 0xFF 0x00 0x00 255)
+    fillCircle (V2 1000 1000, 200) (PixelRGBA8 0x00 0x00 0xFC 255)
+    drawTriangle (V2 700 100, V2 300 100, V2 1800 1100) 3 (PixelRGBA8 0xFF 0x00 0x00 255) Solid
+    fillTriangle (V2 700 100, V2 300 100, V2 1800 1100) (PixelRGBA8 0xFF 0x22 0xAA 255)
+    drawTriangles [(V2 1100 100, V2 1800 100, V2 1800 1100), (V2 1200 100, V2 1900 100, V2 1900 1200)] 3 (PixelRGBA8 0xFF 0x00 0x00 255) Solid
+    fillTriangles [(V2 1100 100, V2 1800 100, V2 1800 1100), (V2 1200 100, V2 1900 100, V2 1900 1200)] (PixelRGBA8 0x22 0x22 0x35 255)
--- src/Drawer.hs
+++ src/Drawer.hs
@@ -1,5 +1,13 @@
 module Drawer (
   drawBackground,
+  fillCircle,
+  drawCircle,
+  fillTriangles,
+  drawTriangles,
+  fillTriangle,
+  drawTriangle,
+  fillRectangle,
+  drawRectangle,
   drawLines,
   drawLine,
   drawPoints,
@@ -12,6 +20,7 @@ import Graphics.Rasterific.Texture
 import Codec.Picture(PixelRGBA8( .. ), Image)
 
 type Size = Float
+type Radius = Float
 data LineType = Dashed | Solid deriving (Eq, Ord, Show)
 
 drawBackground :: Drawing PixelRGBA8 () -> Image PixelRGBA8
@@ -22,15 +31,45 @@ drawBackground =
       height          = 2000
   in renderDrawing width height backgroundColor . withTexture (uniformTexture drawColor)
 
+fillCircle :: (V2 Float, Radius) -> PixelRGBA8 -> Drawing PixelRGBA8 ()
+fillCircle (pc, r) color =
+  withTexture (uniformTexture color) $ fill $ circle pc r
+
+drawCircle :: (V2 Float, Radius) -> Size -> PixelRGBA8 -> Drawing PixelRGBA8 ()
+drawCircle (pc, r) size color =
+  withTexture (uniformTexture color) $ stroke size JoinRound (CapRound, CapRound) $ circle pc r
+
+fillTriangles :: [(V2 Float, V2 Float, V2 Float)] -> PixelRGBA8 -> Drawing PixelRGBA8 ()
+fillTriangles = mconcat . map fillTriangle
+
+drawTriangles :: [(V2 Float, V2 Float, V2 Float)] -> Size -> PixelRGBA8 -> LineType -> Drawing PixelRGBA8 ()
+drawTriangles = mconcat . map drawTriangle
+
+fillTriangle :: (V2 Float, V2 Float, V2 Float) -> PixelRGBA8 -> Drawing PixelRGBA8 ()
+fillTriangle (point1, point2, point3) color =
+  withTexture (uniformTexture color) $ fill [Line point1 point2, Line point2 point3, Line point3 point1]
+
+drawTriangle :: (V2 Float, V2 Float, V2 Float) -> Size -> PixelRGBA8 -> LineType -> Drawing PixelRGBA8 ()
+drawTriangle (point1, point2, point3) =
+  drawLines [(point1, point2), (point2, point3), (point3, point1)]
+
+fillRectangle :: (V2 Float, V2 Float) -> PixelRGBA8 -> Drawing PixelRGBA8 ()
+fillRectangle (V2 x1 y1, V2 x2 y2) color =
+  withTexture (uniformTexture color) $ fill $ rectangle (V2 x1 y1) (x2-x1) (y2-y1)
+
+drawRectangle :: (V2 Float, V2 Float) -> Size -> PixelRGBA8 -> LineType -> Drawing PixelRGBA8 ()
+drawRectangle (V2 x1 y1, V2 x2 y2) =
+  drawLines [(V2 x1 y1, V2 x2 y1), (V2 x2 y1, V2 x2 y2), (V2 x2 y2, V2 x1 y2), (V2 x1 y2, V2 x1 y1)]
+
 drawLines :: [(V2 Float, V2 Float)] -> Size -> PixelRGBA8 -> LineType -> Drawing PixelRGBA8 ()
 drawLines = mconcat . map drawLine
 
 drawLine :: (V2 Float, V2 Float) -> Size -> PixelRGBA8 -> LineType -> Drawing PixelRGBA8 ()
-drawLine (point0, point1) size color linetype = 
+drawLine (point1, point2) size color linetype = 
   withTexture (uniformTexture color) $
   case linetype of {
-    Solid -> stroke size JoinRound (CapRound, CapRound) $ line point0 point1;
-    Dashed -> dashedStroke [5, 10, 5] size JoinRound (CapRound, CapRound) $ line point0 point1
+    Solid -> stroke size JoinRound (CapRound, CapRound) $ line point1 point2;
+    Dashed -> dashedStroke [5, 10, 5] size JoinRound (CapRound, CapRound) $ line point1 point2
   }
 
 drawPoints :: [V2 Float] -> Size -> PixelRGBA8 -> Drawing PixelRGBA8 ()
@@ -44,3 +83,7 @@ drawPoint point size color = withTexture (uniformTexture color) $ fill $ circle 
 -- drawLine (V2 300 300, V2 400 400) 1 (PixelRGBA8 0xFF 0x00 0x00 255) Solid
 -- drawLine (V2 300 300, V2 400 300) 1 (PixelRGBA8 0xFF 0x00 0x00 255) Dashed
 -- drawLines [(V2 600 600, V2 700 700), (V2 800 800, V2 900 900)] 3 (PixelRGBA8 0xFF 0x00 0x00 255) Dashed
+-- drawRectangle (V2 500 500, V2 500 600, V2 600 600, V2 600 500) 3 (PixelRGBA8 0xFF 0x00 0x00 255) Solid
+-- drawCircle (V2 1000 1000, 200) 3 (PixelRGBA8 0xFF 0x00 0x00 255)
+-- drawTriangle (V2 700 100, V2 300 100, V2 1800 1100) 3 (PixelRGBA8 0xFF 0x00 0x00 255) Solid
+-- drawTriangles [(V2 1100 100, V2 1800 100, V2 1800 1100), (V2 1200 100, V2 1900 100, V2 1900 1200)] 3 (PixelRGBA8 0xFF 0x00 0x00 255) Solid
