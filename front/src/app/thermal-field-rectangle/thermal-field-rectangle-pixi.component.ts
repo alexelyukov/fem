508af6943f683d954abfe55072fa36bbee898ea8
Alexelyukov <a.elyukov@timeweb.ru>
2021-01-15 01:55:37 +0300

    fix

--- front/src/app/thermal-field-rectangle/thermal-field-rectangle-pixi.component.ts
+++ front/src/app/thermal-field-rectangle/thermal-field-rectangle-pixi.component.ts
@@ -1,6 +1,6 @@
 import { OnInit, Component, ElementRef, Input, NgZone, OnDestroy } from '@angular/core';
 import * as PIXI from 'pixi.js';
-import { drawLine, drawLines, drawPoint, drawPoints } from '../utils';
+import { drawTest } from '../utils';
 
 @Component({
   selector: 'app-pixi',
@@ -23,19 +23,10 @@ export class PIXIComponent implements OnInit, OnDestroy {
     });
     this.elementRef.nativeElement.appendChild(this.app.view);
 
-    drawPoint(this.app, {x: 100, y: 100}, 1, 0x000000)
-    drawPoint(this.app, {x: 200, y: 100}, 2, 0x0000FF)
-    drawPoint(this.app, {x: 300, y: 100}, 3, 0x00FF00)
-    drawPoint(this.app, {x: 400, y: 100}, 4, 0x00FFFF)
-    drawPoints(this.app, [{x: 500, y: 100}, {x: 600, y: 100}, {x: 700, y: 100}], 4, 0xFF0000)
+    
 
-    drawLine(this.app, {p1: {x: 100, y: 200}, p2: {x: 150, y: 200}}, 1, 0x000000);
-    drawLine(this.app, {p1: {x: 200, y: 200}, p2: {x: 250, y: 200}}, 2, 0x000000);
+    drawTest(this.app);
 
-    drawLines(this.app, [
-      {p1: {x: 400, y: 200}, p2: {x: 450, y: 200}},
-      {p1: {x: 500, y: 200}, p2: {x: 550, y: 200}}
-    ], 2, 0xFF0000);
   }
 
   ngOnInit(): void {
--- front/src/app/utils.ts
+++ front/src/app/utils.ts
@@ -10,6 +10,124 @@ interface Line {
     p2: Point;
 }
 
+interface Rectangle {
+    leftTop: Point;
+    rightBottom: Point;
+}
+
+interface Circle {
+    x: number;
+    y: number;
+    r: number;
+}
+
+interface Triangle {
+    p1: Point;
+    p2: Point;
+    p3: Point;
+}
+
+type Polygon = Point[];
+
+export function drawTest(app: PIXI.Application) {
+    drawPoint(app, {x: 100, y: 100}, 1, 0x000000)
+    drawPoint(app, {x: 200, y: 100}, 2, 0x0000FF)
+    drawPoint(app, {x: 300, y: 100}, 3, 0x00FF00)
+    drawPoint(app, {x: 400, y: 100}, 4, 0x00FFFF)
+    drawPoints(app, [{x: 500, y: 100}, {x: 600, y: 100}, {x: 700, y: 100}], 4, 0xFF0000)
+
+    drawLine(app, {p1: {x: 100, y: 200}, p2: {x: 150, y: 200}}, 1, 0x000000);
+    drawLine(app, {p1: {x: 200, y: 200}, p2: {x: 250, y: 200}}, 2, 0x000000);
+
+    drawLines(app, [
+      {p1: {x: 400, y: 200}, p2: {x: 450, y: 200}},
+      {p1: {x: 500, y: 200}, p2: {x: 550, y: 200}}
+    ], 2, 0xFF0000);
+
+    drawRectangle(app, {leftTop: {x: 100, y: 250}, rightBottom: {x: 150, y: 300}}, 1, 0x000000)
+    drawRectangle(app, {leftTop: {x: 200, y: 250}, rightBottom: {x: 250, y: 300}}, 1, 0x000000, 0xCCCCCC)
+
+    drawRectangle(app, {leftTop: {x: 300, y: 250}, rightBottom: {x: 350, y: 300}}, 1, 0x000000)
+    drawPoints(app, [{x: 300, y: 250}, {x: 350, y: 250}, {x: 350, y: 300}, {x: 300, y: 300}], 2.5, 0xFF0000)
+
+    drawRectangle(app, {leftTop: {x: 400, y: 250}, rightBottom: {x: 450, y: 300}}, 1, 0x000000, 0xCCCCCC)
+    drawPoints(app, [{x: 400, y: 250}, {x: 450, y: 250}, {x: 450, y: 300}, {x: 400, y: 300}], 2.5, 0xFF0000)
+
+    drawCircle(app, {x: 125, y: 350, r: 25}, 1, 0x000000)
+    drawCircle(app, {x: 225, y: 350, r: 25}, 1, 0x000000, 0xCCCCCC)
+
+    drawCircle(app, {x: 325, y: 350, r: 25}, 1, 0x000000)
+    drawPoint(app, {x: 325, y: 350}, 2.5, 0xFF0000)
+
+    drawCircle(app, {x: 425, y: 350, r: 25}, 1, 0x000000, 0xCCCCCC)
+    drawPoint(app, {x: 425, y: 350}, 2.5, 0xFF0000)
+
+    drawTriangle(app, {p1: {x: 100, y: 425}, p2: {x: 150, y: 425}, p3: {x: 125, y: 450}}, 1, 0x000000)
+    drawTriangle(app, {p1: {x: 225, y: 425}, p2: {x: 200, y: 450}, p3: {x: 250, y: 450}}, 1, 0x000000, 0xCCCCCC)
+
+    drawPolygon(app, [{x: 100, y: 525}, {x: 125, y: 475}, {x: 150, y: 525}, {x: 125, y: 550}], 1, 0x000000, 0xCCCCCC)
+}
+
+export function drawTriangles(app: PIXI.Application, triangles: Triangle[], size: number, colorBorder: number , colorFill?: number) {
+    for (const triangle of triangles) {
+        drawTriangle(app, triangle, size, colorBorder, colorFill);
+    }
+}
+
+export function drawTriangle(app: PIXI.Application, triangle: Triangle, size: number, colorBorder: number , colorFill?: number) {
+    drawPolygon(app, [triangle.p1, triangle.p2, triangle.p3], size, colorBorder , colorFill);
+}
+
+export function drawPolygons(app: PIXI.Application, polygons: Polygon[], size: number, colorBorder: number , colorFill?: number) {
+    for (const polygon of polygons) {
+        drawPolygon(app, polygon, size, colorBorder, colorFill);
+    }
+}
+
+export function drawPolygon(app: PIXI.Application, polygon: Polygon, size: number, colorBorder: number , colorFill?: number) {
+    let figure = new PIXI.Graphics();
+    figure.lineStyle(size, colorBorder, 1);
+    if (colorFill) {
+        figure.beginFill(colorFill);
+    }
+
+    let points = [];
+    for (const point of polygon) {
+        points.push(point.x);
+        points.push(point.y);
+    }
+    figure.drawPolygon(points);
+    figure.endFill();
+    app.stage.addChild(figure);
+}
+
+export function drawCircle(app: PIXI.Application, circle: Circle, size: number, colorBorder: number , colorFill?: number) {
+    let figure = new PIXI.Graphics();
+    figure.lineStyle(size, colorBorder, 1);
+    if (colorFill) {
+        figure.beginFill(colorFill);
+    }
+    figure.drawCircle(circle.x, circle.y, circle.r);
+    figure.endFill();
+    app.stage.addChild(figure);
+}
+
+export function drawRectangle(app: PIXI.Application, rectangle: Rectangle, size: number, colorBorder: number , colorFill?: number) {
+    let figure = new PIXI.Graphics();
+    figure.lineStyle(size, colorBorder, 1);
+    if (colorFill) {
+        figure.beginFill(colorFill);
+    }
+    figure.drawRect(
+        rectangle.leftTop.x,
+        rectangle.leftTop.y,
+        rectangle.rightBottom.x - rectangle.leftTop.x,
+        rectangle.rightBottom.y - rectangle.leftTop.y
+    );
+    figure.endFill();
+    app.stage.addChild(figure);
+}
+
 export function drawLines(app: PIXI.Application, lines: Line[], size: number, color: number) {
     for (const line of lines) {
         drawLine(app, line, size, color);
